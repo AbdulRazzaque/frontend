7c710647a203552c1cd62866369436f6abd4c2fd
AbdurRazzaque98 <107128032+AbdurRazzaque98@users.noreply.github.com>
2022-12-15 13:14:04 +0530

    12/15/2022:1:13

--- src/component/admin/AdminLogin.jsx
+++ src/component/admin/AdminLogin.jsx
@@ -6,15 +6,14 @@ const AdminLogin = () => {
     <div>
   <section className="bg-gray-50 ">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-gray-900 ">
-      <img className="w-28 h-12 mr-6" src={logo} alt="logo"/>
-
-          Admin Login  
+  <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-gray-900 ">
+  <img className="w-60 h-20 mr-6" src={logo} alt="logo"/>
+         
       </a>
       <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-10 dark:border-gray-600">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                  Sign in to your account
+              <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                 Admin login
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                   <div>
--- src/component/admin/AdminPanel.jsx
+++ src/component/admin/AdminPanel.jsx
@@ -7,14 +7,14 @@ const AdminPanel = () => {
     <div>
        <section className="bg-gray-50 ">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-gray-900 ">
-      <img className="w-28 h-12 mr-6" src={logo} alt="logo"/>
-          Admin Panel  
+  <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-gray-900 ">
+  <img className="w-60 h-20 mr-6" src={logo} alt="logo"/>
+         
       </a>
       <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gray-10 dark:border-gray-600">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Add group or Company
+              <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                 Admin Panel
               </h1>
               <form className="w-full max-w-lg">
   <div className="flex flex-wrap -mx-3 mb-6">
