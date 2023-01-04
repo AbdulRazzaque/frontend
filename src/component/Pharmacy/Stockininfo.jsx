57439b372470c472bd753667485eba16f607a7d5
AbdurRazzaque98 <107128032+AbdurRazzaque98@users.noreply.github.com>
2023-01-04 13:08:07 +0530

    1/4/20233

--- src/component/Pharmacy/Stockininfo.jsx
+++ src/component/Pharmacy/Stockininfo.jsx
@@ -149,13 +149,33 @@ const Stockininfo = () => {
   return (
     <div className=''>
         <InventoryNavbar/>
-          <h1 className='text-center my-8 font-bold text-2xl'>Stock Info</h1>
+          <h1 className='text-center my-8 font-bold text-2xl'>Stock Info </h1>
         <Container>
         <Stack direction="row" spacing={2} flex justifyContent="center">
-      <TextField type="number" sx={{width:200}} id="outlined-basic" label="Supplier Doc No" variant="outlined"  />
+      {/* <TextField type="number" sx={{width:200}} id="outlined-basic" label="Supplier Doc No" variant="outlined"  />
       <TextField type="Date" sx={{width:200}} id="outlined-basic" label="" variant="outlined"  />
-      <TextField type="text" sx={{width:200}} id="outlined-basic" label="Supplier Name" variant="outlined"  />
-
+      <TextField type="text" sx={{width:200}} id="outlined-basic" label="Supplier Name" variant="outlined"  /> */}
+<div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+      Supplier Doc No
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Supplier Doc No"/>
+      
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+      Date
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Date"/>
+    </div>
+    <div class="w-full  px-3 ">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+      Supplier Name
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Supplier Name"/>
+    </div>
+  </div>
 
 
     </Stack>
--- src/component/Pharmacy/Stockoutinfo.jsx
+++ src/component/Pharmacy/Stockoutinfo.jsx
@@ -152,10 +152,24 @@ const Stockoutinfo = () => {
           <h1 className='text-center my-8 font-bold text-2xl'>Stock Out info </h1>
         <Container>
         <Stack direction="row" spacing={2} flex justifyContent="center">
-      <TextField type="number" sx={{width:200}} id="outlined-basic" label="Supplier Doc No" variant="outlined"  />
-      <TextField type="text" sx={{width:200}} id="outlined-basic" label="Date" variant="outlined"  />
+      {/* <TextField type="number" sx={{width:200}} id="outlined-basic" label="Supplier Doc No" variant="outlined"  />
+      <TextField type="text" sx={{width:200}} id="outlined-basic" label="Date" variant="outlined"  /> */}
 
-
+<div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+      Supplier Doc No
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Supplier Doc No"/>
+      
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+      Date
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Date"/>
+    </div>
+  </div>
 
     </Stack>
     <Stack direction="row" spacing={2} mt="10px">
